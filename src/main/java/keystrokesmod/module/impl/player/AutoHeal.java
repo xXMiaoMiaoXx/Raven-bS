91ec9d0d25d3a658a20376085699b34ab7f5d08b
xXMiaoMiaoXx <3775579896@qq.com>
2024-07-03 05:10:04 +0800

    Add files via upload

--- src/main/java/keystrokesmod/module/impl/player/AutoHeal.java
+++ src/main/java/keystrokesmod/module/impl/player/AutoHeal.java
@@ -1,64 +1,101 @@
 package keystrokesmod.module.impl.player;
 
+import akka.japi.Pair;
+import keystrokesmod.Raven;
+import keystrokesmod.event.*;
 import keystrokesmod.module.Module;
-import keystrokesmod.module.setting.impl.DescriptionSetting;
+import keystrokesmod.module.ModuleManager;
+import keystrokesmod.module.impl.combat.AutoClicker;
+import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
-import keystrokesmod.utility.ContainerUtils;
-import keystrokesmod.utility.Utils;
+import keystrokesmod.script.classes.Vec3;
+import keystrokesmod.utility.*;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.item.ItemSkull;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.*;
+import net.minecraft.network.play.server.S2FPacketSetSlot;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.input.Mouse;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static net.minecraft.util.EnumFacing.DOWN;
 
 public class AutoHeal extends Module {
-    private final SliderSetting minHealth;
-    private final SliderSetting healDelay;
-    private final SliderSetting startDelay;
-    private final SliderSetting switchBackDelay;
-    private long lastHeal = -1;
-    private long lastSwitchTo = -1;
-    private long lastDoneUse = -1;
-    private int lastSlot = -1;
+    private final SliderSetting maxHealth;
+	private final SliderSetting rescueRange;
+	public static boolean isRescuing;
+	public static boolean isKey1Pressed;
+	public static boolean isBlocking;
+	private static boolean foundRescueTarget;
+	
     public AutoHeal() {
-        super("AutoHeal", category.player);
-        this.registerSetting(new DescriptionSetting("help you win Hypixel BUhc."));
-        this.registerSetting(minHealth = new SliderSetting("Min health", 10, 0, 20, 1));
-        this.registerSetting(healDelay = new SliderSetting("Heal delay", 500, 0, 1500, 1));
-        this.registerSetting(startDelay = new SliderSetting("Start delay", 30, 0, 300, 1));
-        this.registerSetting(switchBackDelay = new SliderSetting("Switch back delay", 40, 0, 300, 1));
+        super("AutoRescue", category.player);
+        //this.registerSetting(new DescriptionSetting("Help you rescue teamate in zombie game."));
+        this.registerSetting(maxHealth = new SliderSetting("Rescue when health above", 6, 0, 20, 1));
+		this.registerSetting(rescueRange = new SliderSetting("Rescue range", 2.0, 1.0, 3.0, 0.1));
     }
+	
+	public void onEnable() {
+		isRescuing = false;
+    }
+
+    public void onDisable() {
+		//mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SNEAKING));
+		isRescuing = false;
+		//mc.thePlayer.sendChatMessage("Stop Snaking.");
+    }
+	
+	private void setFoundRescueTarget(boolean val){ foundRescueTarget=val; }
 
     @SubscribeEvent
-    public void onRender(TickEvent.RenderTickEvent event) {
-       if (!Utils.nullCheck() || mc.thePlayer.isDead) return;
-       if (System.currentTimeMillis() - lastHeal < healDelay.getInput()) return;
-
-       if (mc.thePlayer.getHealth() <= minHealth.getInput()) {
-           if (lastSwitchTo == -1) {
-               int toSlot = ContainerUtils.getSlot(ItemSkull.class);
-               if (toSlot == -1) return;
-
-               lastSlot = mc.thePlayer.inventory.currentItem;
-               mc.thePlayer.inventory.currentItem = toSlot;
-               lastSwitchTo = System.currentTimeMillis();
-           }
-       }
-
-       if (lastSwitchTo != -1) {
-           if (lastDoneUse == -1) {
-               if (System.currentTimeMillis() - lastSwitchTo < startDelay.getInput()) return;
-               KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), true);
-               lastDoneUse = System.currentTimeMillis();
-           } else {
-               if (System.currentTimeMillis() - lastDoneUse < switchBackDelay.getInput()) return;
-               KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
-               mc.thePlayer.inventory.currentItem = lastSlot;
-
-               lastSwitchTo = -1;
-               lastDoneUse = -1;
-               lastSlot = -1;
-               lastHeal = System.currentTimeMillis();
-           }
-       }
-    }
+    public void onPreUpdate(PreUpdateEvent e) {
+		foundRescueTarget = false;
+		
+		mc.theWorld.loadedEntityList.stream()
+                .filter(Objects::nonNull)
+                .filter(entity -> entity != mc.thePlayer)
+                .filter(entity -> entity instanceof EntityLivingBase)
+				.map(entity -> new Pair<>(entity, mc.thePlayer.getDistanceSqToEntity(entity)))
+				.sorted((p1, p2) -> p2.second().compareTo(p1.second()))
+				.forEach(pair -> {
+                    // need a more accurate distance check as this can ghost on hypixel
+                    if (pair.first() instanceof EntityPlayer && pair.second() <= rescueRange.getInput() && ((EntityLivingBase)pair.first()).isPlayerSleeping()) {
+						setFoundRescueTarget(true);
+                    }
+                });
+		if(foundRescueTarget){
+			AutoClicker.rescuing = true;
+			if(!isRescuing && mc.thePlayer.getHealth()>=maxHealth.getInput()){
+				//mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING));
+				//mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING));
+				if (Utils.nullCheck() && mc.inGameHasFocus) 
+					KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), true);
+				isRescuing = true;
+				//mc.thePlayer.sendChatMessage("Start Snaking.");
+			}
+		}else{
+			if(isRescuing){
+				//mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SNEAKING));
+				AutoClicker.rescuing = false;
+				isRescuing = false;
+				KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), false);
+				KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
+				mc.thePlayer.inventory.currentItem = 4;
+				//mc.thePlayer.sendChatMessage("Stop Snaking.");
+			}
+		}
+		
+	}
 }
